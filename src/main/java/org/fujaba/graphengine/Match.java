d642cbb3e182790b12978b8267dbe6c4ccc4292b
hoechp <send-a-mail@web.de>
2016-12-02 00:17:07 +0100

    #10 created Match class and implemented applying matches
    still missing the most essential matching itself though.

--- src/main/java/org/fujaba/graphengine/Match.java
+++ src/main/java/org/fujaba/graphengine/Match.java
@@ -1,5 +1,12 @@
 package org.fujaba.graphengine;
 
+import java.util.HashMap;
+
+import org.fujaba.graphengine.graph.Graph;
+import org.fujaba.graphengine.graph.Node;
+import org.fujaba.graphengine.pattern.PatternGraph;
+import org.fujaba.graphengine.pattern.PatternNode;
+
 /**
  * A Match is a single match of a PatternGraph in a Graph.
  * Every PatternNode, PatternEdge and PatternAttribute is mapped to a Node,
@@ -9,4 +16,48 @@ package org.fujaba.graphengine;
  */
 public class Match {
 
+	/**
+	 * the graph, this match did match on
+	 */
+	private Graph graph;
+	/**
+	 * the pattern that was matched
+	 */
+	private PatternGraph pattern;
+	/**
+	 * the mapping from pattern nodes to graph nodes
+	 */
+	private HashMap<PatternNode, Node> nodeMatch;
+
+	/**
+	 * the constructor for this Match.
+	 * @param graph the graph, this match did match on
+	 * @param pattern the pattern that was matched
+	 * @param nodeMatch the mapping from pattern nodes to graph nodes
+	 */
+	public Match(Graph graph, PatternGraph pattern, HashMap<PatternNode, Node> nodeMatch) {
+		this.graph = graph;
+		this.pattern = pattern;
+		this.nodeMatch = nodeMatch;
+	}
+	
+	public Graph getGraph() {
+		return graph;
+	}
+	public void setGraph(Graph graph) {
+		this.graph = graph;
+	}
+	public PatternGraph getPattern() {
+		return pattern;
+	}
+	public void setPattern(PatternGraph pattern) {
+		this.pattern = pattern;
+	}
+	public HashMap<PatternNode, Node> getNodeMatch() {
+		return nodeMatch;
+	}
+	public void setNodeMatch(HashMap<PatternNode, Node> nodeMatch) {
+		this.nodeMatch = nodeMatch;
+	}
+	
 }
--- src/main/java/org/fujaba/graphengine/PatternEngine.java
+++ src/main/java/org/fujaba/graphengine/PatternEngine.java
@@ -1,10 +1,89 @@
 package org.fujaba.graphengine;
 
+import java.util.ArrayList;
+
+import org.fujaba.graphengine.graph.Graph;
+import org.fujaba.graphengine.graph.Node;
+import org.fujaba.graphengine.pattern.PatternAttribute;
+import org.fujaba.graphengine.pattern.PatternEdge;
+import org.fujaba.graphengine.pattern.PatternGraph;
+import org.fujaba.graphengine.pattern.PatternNode;
+
 /**
  * The PatternEngine contains all logic concerning PatternGraphs.
  * 
  * @author Philipp Kolodziej
  */
 public class PatternEngine {
+	
+	/**
+	 * matches and directly applies a pattern as often as possible
+	 * until the optional limit is met, without revisiting graphs,
+	 * that were already used (preventing endless cycles).
+	 * 
+	 * @param graph the graph to apply the pattern on
+	 * @param pattern the pattern to apply
+	 * @param maxCount the maximum number of times the pattern should be applied to the graph
+	 * @return the resulting graph
+	 */
+	public static Graph applyPattern(Graph graph, PatternGraph pattern, int maxCount) { // TODO: implement
+		Graph result = graph.clone();
+		
+		return result;
+	}
 
+	/**
+	 * finds matches for a pattern in a graph.
+	 * 
+	 * @param graph the graph to match the pattern on
+	 * @param pattern the pattern to match
+	 * @return a list of matches for the pattern in the graph
+	 */
+	public static ArrayList<Match> matchPattern(Graph graph, PatternGraph pattern) { // TODO: implement
+		ArrayList<Match> matches = new ArrayList<Match>();
+		
+		return matches;
+	}
+	
+	/**
+	 * applies a pattern to a match, applying all 'create'- and 'delete'-actions specified in the pattern to the graph.
+	 * 
+	 * @param match the match that was previously found
+	 * @return the resulting graph
+	 */
+	public static Graph applyMatch(Match match) {
+		for (PatternNode patternNode: match.getPattern().getPatternNodes()) {
+			Node matchedNode;
+			switch (patternNode.getAction()) {
+			case "remove": // remove
+				match.getGraph().removeNode(match.getNodeMatch().get(patternNode));
+				continue;
+			case "create": // create
+				matchedNode = new Node();
+				break;
+			default: // match
+				matchedNode = match.getNodeMatch().get(patternNode);
+			}
+			for (PatternAttribute patternAttribute: patternNode.getPatternAttributes()) {
+				switch (patternAttribute.getAction()) {
+				case "remove": // remove
+					matchedNode.removeAttribute(patternAttribute.getName());
+					break;
+				case "create": // create
+					matchedNode.setAttribute(patternAttribute.getName(), patternAttribute.getValue());
+				}
+			}
+			for (PatternEdge patternEdge: patternNode.getPatternEdges()) {
+				switch (patternEdge.getAction()) {
+				case "remove": // remove
+					matchedNode.removeEdge(patternEdge.getName(), match.getNodeMatch().get(patternEdge.getTarget()));
+					break;
+				case "create": // create
+					matchedNode.addEdge(patternEdge.getName(), match.getNodeMatch().get(patternEdge.getTarget()));
+				}
+			}
+		}
+		return match.getGraph();
+	}
+	
 }
