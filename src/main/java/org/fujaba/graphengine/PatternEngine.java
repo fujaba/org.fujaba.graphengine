186d4710b7ec30c8abdb66a2b01c0dd6682219e9
hoechp <send-a-mail@web.de>
2016-12-03 01:50:25 +0100

    #9 #10 implemented method to find matches for patterns in graphs

--- src/main/java/org/fujaba/graphengine/PatternEngine.java
+++ src/main/java/org/fujaba/graphengine/PatternEngine.java
@@ -1,6 +1,8 @@
 package org.fujaba.graphengine;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
 import org.fujaba.graphengine.graph.Graph;
 import org.fujaba.graphengine.graph.Node;
@@ -39,9 +41,95 @@ public class PatternEngine {
 	 * @param pattern the pattern to match
 	 * @return a list of matches for the pattern in the graph
 	 */
-	public static ArrayList<Match> matchPattern(Graph graph, PatternGraph pattern) { // TODO: implement
+	public static ArrayList<Match> matchPattern(Graph graph, PatternGraph pattern) {
 		ArrayList<Match> matches = new ArrayList<Match>();
-		
+		// step 1: for every PatternNode, find all possible Nodes:
+		ArrayList<ArrayList<Node>> couldMatch = new ArrayList<ArrayList<Node>>();
+		for (int i = 0; i < pattern.getPatternNodes().size(); ++i) {
+			PatternNode patternNode = pattern.getPatternNodes().get(i);
+			if (patternNode.getAction() == "create") {
+				continue;
+			}
+			couldMatch.add(new ArrayList<Node>());
+nodes:		for (int j = 0; j < graph.getNodes().size(); ++j) {
+				Node node = graph.getNodes().get(j);
+				for (int k = 0; k < patternNode.getPatternAttributes().size(); ++k) {
+					PatternAttribute patternAttribute = patternNode.getPatternAttributes().get(k);
+					if (patternAttribute.getAction() == "create") {
+						continue;
+					}
+					boolean attributeValueMatch = patternAttribute.getValue().equals(node.getAttribute(patternAttribute.getName()));
+					if ((patternAttribute.isNegative() && attributeValueMatch) || (!patternAttribute.isNegative() && !attributeValueMatch)) {
+						continue nodes;
+					}
+				}
+				for (int k = 0; k < patternNode.getPatternEdges().size(); ++k) {
+					PatternEdge patternEdge = patternNode.getPatternEdges().get(k);
+					if (patternEdge.getAction() == "create" || patternEdge.isNegative()) {
+						continue;
+					}
+					if (!node.getEdges().containsKey(patternEdge.getName())) {
+						continue nodes;
+					}
+				}
+				couldMatch.get(i).add(node);
+			}
+			if (couldMatch.get(i).size() < 1) {
+				return matches;
+			}
+		}
+		// step 2: verify the possible matches
+		boolean canTryAnother = false;
+		ArrayList<Integer> currentTry = new ArrayList<Integer>();
+		for (int i = 0; i < couldMatch.size(); ++i) {
+			currentTry.add(0);
+		}
+		do {
+			canTryAnother = false;
+			HashMap<PatternNode, Node> mapping = new HashMap<PatternNode, Node>(); 
+			HashSet<Node> usedNodes = new HashSet<Node>();
+			boolean duplicateChoice = false;
+			for (int i = 0; i < couldMatch.size(); ++i) {
+				PatternNode patternNode = pattern.getPatternNodes().get(i);
+				Node node = couldMatch.get(i).get(currentTry.get(i));
+				mapping.put(patternNode, node);
+				if (usedNodes.contains(node)) {
+					duplicateChoice = true;
+					break;
+				}
+				usedNodes.add(node);
+			}
+			if (!duplicateChoice) {
+				boolean fail = false;
+nodes:			for (int i = 0; i < pattern.getPatternNodes().size(); ++i) {
+					PatternNode sourcePatternNode = pattern.getPatternNodes().get(i);
+					for (int j = 0; j < sourcePatternNode.getPatternEdges().size(); ++j) {
+						PatternEdge patternEdge = sourcePatternNode.getPatternEdges().get(j);
+						if (patternEdge.getAction() == "create") {
+							continue;
+						}
+						boolean isThere = mapping.get(sourcePatternNode).getEdges(patternEdge.getName()).contains(mapping.get(patternEdge.getTarget()));
+						if ((patternEdge.isNegative() && isThere) || (!patternEdge.isNegative() && !isThere)) {
+							fail = true;
+							break nodes;
+						}
+					}
+				}
+				if (!fail) {
+					matches.add(new Match(graph, pattern, mapping));
+				}
+			}
+			for (int i = 0; i < currentTry.size(); ++i) {
+				if (currentTry.get(i) < couldMatch.get(i).size() - 1) {
+					currentTry.set(i, currentTry.get(i) + 1);
+					for (int j = 0; j < i; ++j) {
+						currentTry.set(j, 0);
+					}
+					canTryAnother = true;
+					break;
+				}
+			}
+		} while (canTryAnother);
 		return matches;
 	}
 	
